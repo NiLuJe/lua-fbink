c606ecae66e22c268018aa771f6b00d70c51f5a2
NiLuJe <ninuje@gmail.com>
2019-06-26 17:46:27 +0200

    CamelCase the FBInk namespace

--- hello.lua
+++ hello.lua
@@ -11,12 +11,12 @@ local _ = require("ffi/fbink_h")
 
 -- And load the actual FBInk library...
 -- Either by specifying a filepath:
---local fbink = ffi.load("lib/libfbink.so.1.0.0")
+--local FBInk = ffi.load("lib/libFBInk.so.1.0.0")
 -- Or by letting the dynamic loader figure it out ;).
-local fbink = ffi.load("fbink")
+local FBInk = ffi.load("fbink")
 
 -- Let's check which FBInk version we're using...
-print("Loaded FBInk " .. ffi.string(fbink.fbink_version()))
+print("Loaded FBInk " .. ffi.string(FBInk.fbink_version()))
 
 -- And now we're good to go! Let's print "Hello World" in the center of the screen...
 -- Setup the config...
@@ -25,25 +25,25 @@ fbink_cfg[0].is_centered = true
 fbink_cfg[0].is_halfway = true
 
 -- Open the FB...
-local fbfd = fbink.fbink_open()
+local fbfd = FBInk.fbink_open()
 if fbfd == -1 then
 	print("Failed to open the framebuffer, aborting . . .")
 	os.exit(-1)
 end
 
 -- Initialize FBInk...
-if fbink.fbink_init(fbfd, fbink_cfg) < 0 then
+if FBInk.fbink_init(fbfd, fbink_cfg) < 0 then
 	print("Failed to initialize FBInk, aborting . . .")
 	os.exit(-1)
 end
 
 -- Do stuff!
-if fbink.fbink_print(fbfd, "Hello World", fbink_cfg) < 0 then
+if FBInk.fbink_print(fbfd, "Hello World", fbink_cfg) < 0 then
 	print("Failed to print that string!")
 end
 
 -- And now we can wind things down...
-if fbink.fbink_close(fbfd) < 0 then
+if FBInk.fbink_close(fbfd) < 0 then
 	print("Failed to close the framebuffer, aborting . . .")
 	os.exit(-1)
 end
