f01aea16ac22fe27ba72e7642479a7072723bb35
NiLuJe <ninuje@gmail.com>
2021-06-13 18:45:55 +0200

    Prettier syntax ;)

--- hello.lua
+++ hello.lua
@@ -20,9 +20,13 @@ print("Loaded FBInk " .. ffi.string(FBInk.fbink_version()))
 
 -- And now we're good to go! Let's print "Hello World" in the center of the screen...
 -- Setup the config...
-local fbink_cfg = ffi.new("FBInkConfig[1]")
-fbink_cfg[0].is_centered = true
-fbink_cfg[0].is_halfway = true
+-- NOTE: Because this is a struct, we can rely on an implicit cdata<&FBInkConfig> conversion
+--       when passed to a C function that expects a cdata<FBInkConfig*>.
+--       c.f., https://luajit.org/ext_ffi_semantics.html
+--       This allows a less unwieldy syntax than the usual "array of one" approach necessary for outargs pointers.
+local fbink_cfg = ffi.new("FBInkConfig")
+fbink_cfg.is_centered = true
+fbink_cfg.is_halfway = true
 
 -- Open the FB...
 local fbfd = FBInk.fbink_open()
